645b0cf3675506a8c2fbdabd345902890d9df757
ksh04023 <ksh04023@gmail.com>
2020-05-19 23:37:21 +0900

    project files

---
+++ app/src/main/java/com/example/datacollect_android/Tutorial1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.datacollect_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tutorial1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial1)
+    }
+}
---
+++ app/src/main/java/com/example/datacollect_android/Tutorial2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.datacollect_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tutorial2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial2)
+    }
+}
