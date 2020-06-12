bc877315cee6f90709b290f54bb91541ec552d4d
ksh04023 <ksh04023@gmail.com>
2020-06-12 17:05:52 +0900

    getUsageData

--- app/src/main/java/com/example/datacollect_android/MainActivity.kt
+++ app/src/main/java/com/example/datacollect_android/MainActivity.kt
@@ -173,8 +173,7 @@ class MainActivity : AppCompatActivity() {
         val queryUsageStats = usageStatsManager.queryUsageStats(
             UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis, System.currentTimeMillis()
         )
-
-
+        Log.d("appusing",queryUsageStats.size.toString())
         return queryUsageStats
     }
 
---
+++ app/src/main/java/com/example/datacollect_android/SignInActivity.kt
@@ -0,0 +1,18 @@
+package com.example.datacollect_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+    }
+
+
+
+
+
+
+
+}
