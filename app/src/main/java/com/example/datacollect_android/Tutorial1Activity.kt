0e21fa37829671473bc5e0299ec2bd119b3a63cf
sejin <sejinnnnnn@naver.com>
2020-06-15 21:15:54 +0900

    ggg

--- app/src/main/java/com/example/datacollect_android/Tutorial1Activity.kt
+++ app/src/main/java/com/example/datacollect_android/Tutorial1Activity.kt
@@ -1,21 +1,113 @@
 package com.example.datacollect_android
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_tutorial1.*
+import java.io.File
+import java.io.FileWriter
 
 class Tutorial1Activity : AppCompatActivity() {
+
+    val MULTIPLE_REQUEST = 1234
+
+    var permissionArr = arrayOf(
+        android.Manifest.permission.PACKAGE_USAGE_STATS,
+        android.Manifest.permission.ACCESS_NETWORK_STATE,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.INTERNET,
+        android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+
+    lateinit var fbDatabase: FirebaseDatabase
+    lateinit var dbReference: DatabaseReference
+
+    lateinit var userInfo: UserInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial1)
+
+        initPermission()
+
+        fbDatabase = FirebaseDatabase.getInstance()
+        dbReference = fbDatabase.reference
+
+        val Listener = object: ValueEventListener {
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("T1_FBError", p0.toException().toString())
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (snap in p0.children) {
+                    Log.d("T1_Child", snap.getValue().toString())
+                }
+            }
+
+        }
+
+        fbDatabase.getReference().addValueEventListener(Listener)
+
+        var i = 0
+
+        //data class UserInfo( val userName: String? ="", val userPhone: String? = "", val userCode: Int = 0, val userGender: Int = 0, val userGrade: Int = 0) {
+        testbutton.setOnClickListener {
+            val userInfo = UserInfo("abc", "123123", "123","123",3)
+            dbReference.child("user").push().setValue(userInfo)
+        }
+
         nextButton1.setOnClickListener {
             val intent = Intent(this,Tutorial2Activity::class.java) //다음이어질 액티비티
             startActivity(intent)
             overridePendingTransition(0, 0)
             finish()
+        }
 
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        val file = File("user.txt")
+//        if (file != null) {
+//            finish()
+//        }
+    }
+
+
+    fun initPermission() {
+        var rejectedPermissionList = ArrayList<String>()
+
+        for (permission in permissionArr) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                rejectedPermissionList.add(permission)
+            }
+        }
+        if(rejectedPermissionList.isNotEmpty()) {
+            val array = arrayOfNulls<String>(rejectedPermissionList.size)
+            ActivityCompat.requestPermissions(this, rejectedPermissionList.toArray(array), MULTIPLE_REQUEST)
         }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MULTIPLE_REQUEST -> {
+                Toast.makeText(this, "권한 모두 승인됨", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/datacollect_android/UserInfo.kt
+++ app/src/main/java/com/example/datacollect_android/UserInfo.kt
@@ -1,4 +1,4 @@
 package com.example.datacollect_android
 
-data class UserInfo(val userCode: Int = 0, val userName: String? ="", val userGrade: Int = 0) {
+data class UserInfo( val userName: String? ="", val userPhone: String? = "", val userCode: String? = "", val userGender: String? = "", val userGrade: Int = 0, val uniqueKey: String? ="") {
 }
